8dd3fe7b60fc58a69d0514cf18771d324246936a
xDeja <61637301+xDeja@users.noreply.github.com>
2021-03-18 09:33:54 -0700

    Added Store

---
+++ JavaEssentials.java
@@ -0,0 +1,9 @@
+public class JavaEssentials {
+    public static void main(String[] args){
+        print("Hello");
+    }
+
+    public static void print(Object obj){
+        System.out.println(obj);
+    }
+}
